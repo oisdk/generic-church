425ed29424bd5d5fcdf9889f4cc962349191eb49
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-12-14 22:38:18 +0000

    nicer

--- src/Data/Generic/Church.hs
+++ src/Data/Generic/Church.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DefaultSignatures    #-}
-{-# LANGUAGE EmptyCase            #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE TypeOperators        #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DefaultSignatures      #-}
+{-# LANGUAGE EmptyCase              #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE UndecidableInstances   #-}
+{-# LANGUAGE MagicHash              #-}
 
 module Data.Generic.Church where
 
@@ -12,115 +14,125 @@ module Data.Generic.Church where
 import           Data.Proxy
 import           Data.Void
 import           GHC.Generics
-import           Prelude      hiding (take)
+import           Data.Coerce
+import           GHC.Prim
+
+infixr 9 .#, #.
+
+(.#) :: Coercible a b => (b -> c) -> (a -> b) -> a -> c
+(.#) f _ = coerce f
+
+(#.) :: Coercible b c => (b -> c) -> (a -> b) -> a -> c
+(#.) _ = coerce
 
 absurd1 :: V1 a -> b
-absurd1 = absurd . to . M1
+absurd1 = absurd . to .# M1
 
-class Summable c  where
-    type Sum c a r :: *
-    take :: c p -> (a -> r) -> Sum c a r
-    ignore :: Proxy c -> Proxy a -> r -> Sum c a r
+type family Sum (c :: * -> *) (a :: *) (r :: *) :: * where
+        Sum (M1 i t c) a r = Sum c a r
+        Sum (K1 i c) a r = (c -> a) -> r
+        Sum U1 a r = a -> r
+        Sum V1 a r = r
+        Sum (li :+: ri) a r = Sum li a (Sum ri a r)
+        Sum (li :*: ri) a r = Prod li (Prod ri a) -> r
+
+type family Prod (c :: * -> *) (r :: *) :: * where
+        Prod (M1 i t c) r = Prod c r
+        Prod U1 r = r
+        Prod (K1 i c) r = c -> r
+        Prod V1 r = Void -> r
+        Prod (li :*: ri) a = Prod li (Prod ri a)
+
+type family GenFold (c :: * -> *) (a :: *) :: * where
+        GenFold (M1 i t c) a = GenFold c a
+        GenFold (K1 i c) a = (c -> a) -> a
+        GenFold U1 a = a -> a
+        GenFold (li :+: ri) a = Sum li a (Sum ri a a)
+        GenFold (li :*: ri) a = Prod li (Prod ri a) -> a
+
+class Disj c  where
+    elim :: c p -> (a -> r) -> Sum c a r
+    ignore :: Proxy# (c a) -> r -> Sum c a r
 
 class Prodable c  where
-    type Prod c r :: *
     strip :: c p -> Prod c r -> r
 
 class ChurchGen c  where
-    type FoldGen c a :: *
-    foldGen :: c a -> FoldGen c a
+    foldGen :: c a -> GenFold c a
 
-instance Summable c =>
-         Summable (M1 i t c) where
-    type Sum (M1 i t c) a r = Sum c a r
-    take (M1 x) = take x
-    ignore (_ :: Proxy (M1 i t c)) = ignore (Proxy :: Proxy c)
+instance Disj c =>
+         Disj (M1 i t c) where
+    elim = elim .# unM1
+    ignore (_ :: Proxy (M1 i t c a)) = ignore (Proxy :: Proxy (c a))
 
 instance Prodable c =>
          Prodable (M1 i t c) where
-    type Prod (M1 i t c) r = Prod c r
-    strip (M1 x) = strip x
+    strip = strip .# unM1
 
 instance ChurchGen c =>
          ChurchGen (M1 i t c) where
-    type FoldGen (M1 i t c) a = FoldGen c a
-    foldGen (M1 x) = foldGen x
+    foldGen = foldGen .# unM1
 
-instance Summable (K1 i c) where
-  type Sum (K1 i c) a r = (c -> a) -> r
-  take (K1 x) k f = k (f x)
-  ignore _ _ r _ = r
+instance Disj (K1 i c) where
+  elim (K1 x) k f = k (f x)
+  ignore _ r _ = r
 
-instance Summable U1 where
-    type Sum U1 a r = a -> r
-    take U1 = ($)
-    ignore _ _ r _ = r
+instance Disj U1 where
+    elim U1 = ($)
+    ignore _ r _ = r
 
 instance Prodable U1 where
-    type Prod U1 r = r
     strip U1 x = x
 
 instance ChurchGen U1 where
-    type FoldGen U1 a = a -> a
     foldGen U1 x = x
 
-instance Summable V1 where
-    type Sum V1 a r = r
-    take = absurd1
-    ignore _ _ = id
+instance Disj V1 where
+    elim = absurd1
+    ignore _ = id
 
 instance Prodable V1 where
-    type Prod V1 r = Void -> r
     strip = absurd1
 
 instance Prodable (K1 i c) where
-    type Prod (K1 i c) r = c -> r
     strip (K1 x) f = f x
 
 instance ChurchGen (K1 i c) where
-    type FoldGen (K1 i c) a = (c -> a) -> a
     foldGen (K1 x) f = f x
 
-instance (Summable li, Summable ri) =>
-         Summable (li :+: ri) where
-    type Sum (li :+: ri) a r = Sum li a (Sum ri a r)
-    take (L1 x) (k :: a -> r) =
-        take x (ignore (Proxy :: Proxy ri) (Proxy :: Proxy a) . k)
-    take (R1 x) (k :: a -> r) =
-        ignore (Proxy :: Proxy li) (Proxy :: Proxy a) (take x k)
-    ignore _ a x =
-        ignore (Proxy :: Proxy li) a (ignore (Proxy :: Proxy ri) a x)
+instance (Disj li, Disj ri) =>
+         Disj (li :+: ri) where
+    elim (L1 x) (k :: a -> r) = elim x (ignore (Proxy :: Proxy (ri a)) . k)
+    elim (R1 x) (k :: a -> r) = ignore (Proxy :: Proxy (li a)) (elim x k)
+    ignore (_ :: Proxy ((li :+: ri) a)) x =
+        ignore (Proxy :: Proxy (li a)) (ignore (Proxy :: Proxy (ri a)) x)
 
-instance (Summable li, Summable ri) =>
+instance (Disj li, Disj ri) =>
          ChurchGen (li :+: ri) where
-    type FoldGen (li :+: ri) a = Sum li a (Sum ri a a)
-    foldGen (x :: (li :+: ri) a) = take x (id :: a -> a)
+    foldGen (x :: (li :+: ri) a) = elim x (id :: a -> a)
 
 instance (Prodable li, Prodable ri) =>
          Prodable (li :*: ri) where
-    type Prod (li :*: ri) a = Prod li (Prod ri a)
     strip (li :*: ri) f = strip ri (strip li f)
 
 instance (Prodable li, Prodable ri) =>
          ChurchGen (li :*: ri) where
-    type FoldGen (li :*: ri) a = Prod li (Prod ri a) -> a
     foldGen (li :*: ri) f = strip ri (strip li f)
 
 instance (Prodable li, Prodable ri) =>
-         Summable (li :*: ri) where
-    type Sum (li :*: ri) a r = Prod li (Prod ri a) -> r
-    take x k = k . strip x
-    ignore _ _ r _ = r
+         Disj (li :*: ri) where
+    elim x k = k . strip x
+    ignore _ r _ = r
 
 class Church c  where
     type Fold c a :: *
-    type Fold c a = FoldGen (Rep c) a
+    type Fold c a = GenFold (Rep c) a
     fold :: Proxy a -> c -> Fold c a
     default fold :: (Generic c
                     ,ChurchGen (Rep c)
-                    ,FoldGen (Rep c) a ~ Fold c a) => proxy a -> c -> Fold c a
-    fold = defaultFold
+                    ,GenFold (Rep c) a ~ Fold c a) => proxy a -> c -> Fold c a
+    fold = genericFold
 
-defaultFold :: (Generic c, ChurchGen (Rep c)) => proxy a -> c -> FoldGen (Rep c) a
-defaultFold (_ :: proxy a) (x :: c) = foldGen (from x :: Rep c a)
+genericFold :: (Generic c, ChurchGen (Rep c)) => proxy a -> c -> GenFold (Rep c) a
+genericFold (_ :: proxy a) (x :: c) = foldGen (from x :: Rep c a)
 
